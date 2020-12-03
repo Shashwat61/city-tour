5aa8a99253d4479b4cba98f818a91721214b0281
Shashwat61 <shashwat1461.cse18@chitkara.edu.in>
2020-12-03 11:44:53 +0530

    third commit

--- src/components/tourData.js
+++ src/components/tourData.js
@@ -1,27 +1,27 @@
 export const tourData = [
   {
     id: 1,
-    city: "new york",
+    city: "Alaska",
     img: "./img/img1.jpg",
-    name: "new york bridge tour",
+    name: "Alaska",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "In the northern part of the Alaska Range, Denali National Park is the one of the largest in the United States and encompasses North America's highest mountain. Denali is the 20,320-foot peak's traditional name, but modern explorers dubbed it Mount McKinley."
   },
   {
     id: 2,
-    city: "paris",
+    city: "Germany",
     img: "./img/img2.jpg",
-    name: "paris lights tour",
+    name: "The Ultimate Fairytale Castle: Neuschwanstein",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "The quaint old town of Füssen, situated between the Ammergau and Allgäu Alps and a popular alpine resort and winter sports center, is a good base from which to explore nearby Neuschwanstein Castle, one of Europe's most famous (and picturesque) royal castles."
   },
   {
     id: 3,
-    city: "london",
+    city: "Greece",
     img: "./img/img3.jpg",
-    name: "london royal palace tour",
+    name: "Santorini",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Stunning Santorini is the most dramatic of all the Greek isles. It is best known for the west coast cliff-top towns of Fira and Oia, which appear to hang over a deep, blue sea-filled caldera."
   },
   {
     id: 4,
@@ -29,6 +29,6 @@ export const tourData = [
     img: "./img/img4.jpg",
     name: "tokyo sushi tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "It's hard to miss the Tokyo Skytree (Tōkyō Sukaitsurī). This 634-meter-tall communications and observation tower rises out of the city's Sumida district of Minato like a huge rocket ship"
   }
 ];
